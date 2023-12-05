7b6c559250264b8dc939e2c4a099c472785a48d8
Ekene Muoma <muomaekene@yahoo.com>
2023-12-04 19:19:57 -0500

    Update to typical configuration

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,4 +1,18 @@
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
 export default {
   stackbitVersion: "~0.6.0",
   ssgName: "nextjs",
+  nodeVersion: "16",
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  models: {
+    page: { type: "page", urlPath: "/{slug}" },
+  },
 };
